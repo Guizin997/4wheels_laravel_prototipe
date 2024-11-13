6070b2ee9323e0175dada5f03e2edd832229aed2
Guizin997 <guiguirussas@gmail.com>
2024-11-13 16:28:55 -0300

    adição de rotas

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/cars', [CarController::class,  'index'])->name('cars.index');
+Route::get('/cars/create', [CarController::class,  'create'])->name('cars.create');
+Route::post('/cars', [CarController::class,  'store'])->name('cars.store');
+Route::get('/cars/{car}', [CarController::class,  'show'])->name('cars.show');
+Route::get('/cars/{car}/edit', [CarController::class,  'edit'])->name('cars.edit');
+Route::put('/cars/{car}', [CarController::class,  'update'])->name('cars.update');
+Route::delete('/cars/{car}', [CarController::class,  'destroy'])->name('cars.destroy');
 
 require __DIR__.'/auth.php';
 
